3313565a1493808a8ab73ad5151630982cee8074
sittimani <67500879+sittimani@users.noreply.github.com>
2022-09-21 06:49:14 +0530

    feature c commit -2 is merged with commit 1

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 const object = {
     1 : 'initial commit',
     2 : 'feature a',
-    3 : 'featureb commit'
+    3 : 'featureb commit',
+    4 : 'featurec commit - 1',
+    5 : 'featurec commit - 2',
 }
